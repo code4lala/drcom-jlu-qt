1d60a9786bd86c578ec5a7f43ca744cb69fbff66
ImFengLala <fenglala@outlook.com>
2019-03-13 21:48:46 +0800

    重新适配高dpi

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 
 int main(int argc, char *argv[])
 {
+    SingleApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     Q_INIT_RESOURCE(DrCOM_JLU_Qt);
     SingleApplication a(argc,argv);
     SingleApplication::setQuitOnLastWindowClosed(false);
+
+    QFont font=a.font();
+    font.setPointSize(12);
+    a.setFont(font);
 
     QTranslator translator;
     translator.load(":/ts/DrCOM_zh_CN.qm");
